689325e8d8b31398200918e28940bf199b7a34f4
zentoid <git@zentoid.com>
2020-08-29 10:00:07 +0100

    add list s3 bucket add logging to file

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# s3_search_fuzzer
+# Search fuzzer
+## Simple S3 bucket searcher
+
+This is a very simple script that takes a base bucket name and will prefix and suffix the "fuzz words" using the allowed separators.
+
+
+## Usage is simple
+The simplest working command is
+
+    python s3_seeker.py -b test
+ 
+The main part of the bucket name you are searching for (e.g. CompanyName)
+
+    -b, --base
+
+File tht contains terms to add to the main name above.
+
+Default: fuzz.txt
+
+    -f, --fuzzer
+
+File that contains various separators to use.
+
+Default: separators.txt
+
+    -s, --separator
+
+Write output to console and log file
+
+Default: True
+
+    -w, --write
--- s3_seeker.py
+++ s3_seeker.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Created by zentoid
-
+import logging
 import os
 import random
 from argparse import ArgumentParser
+from datetime import datetime
 from time import sleep
 
+import boto3
+import botocore.exceptions
+import coloredlogs
 import requests
+from botocore import UNSIGNED
+from botocore.config import Config
 
 ENABLE_SLEEP_TIMER = False
 BASE_SLEEP_TIME = 1
@@ -21,9 +27,60 @@ parser = ArgumentParser()
 parser.add_argument("-b", "--base", dest="main_bucket_name_part", help="The main part of the bucket name you are searching for (e.g. CompanyName)", required=True)
 parser.add_argument("-f", "--fuzzer", dest="source_fuzzer_file", help="File tht contains terms to add to the main name above", default="fuzz.txt")
 parser.add_argument("-s", "--separator", dest="source_separators_file", help="File tht contains various separators to use", default="separators.txt")
+parser.add_argument("-w", "--write", dest="write", help="Write output to console and log file", default=True)
 args = parser.parse_args()
 
+log_file_name = str(datetime.strftime(datetime.now(), '%d-%m-%Y_%H-%M-%S.%f'))
+write_to_file = args.write
+
+# Create file logger
+flog = logging.getLogger('s3seeker_file_log')
+flog.setLevel(logging.DEBUG)  # Set log level for logger object
+
+# Create file handler which logs even debug messages
+fh = logging.FileHandler('{}.log'.format(log_file_name))
+fh.setLevel(logging.DEBUG)
+
+# Add the handler to logger
+flog.addHandler(fh)
+
+# Create secondary logger for logging to screen
+stdoutlog = logging.getLogger('s3seeker_file_log_std_out')
+stdoutlog.setLevel(logging.INFO)
+
+# Logging levels for the screen logger:
+#   INFO  = found
+#   ERROR = not found
+# The levels serve no other purpose than to specify the output color
+
+levelStyles = {
+    'debug': {'color': 'orange'},
+    'info': {'color': 'white'},
+    'warning': {'color': 'yellow'},
+    'error': {'color': 'red'}
+}
+
+# this is nice if you want the time in a static color - not using at the moment
+fieldStyles = {
+    'asctime': {'color': 'white'}
+}
+
+# Use coloredlogs to add color to screen logger. Define format and styles.
+# coloredlogs.install(level='DEBUG', logger=stdoutlog, fmt='%(asctime)s - %(message)s', level_styles=levelStyles, field_styles=fieldStyles)
+coloredlogs.install(level='DEBUG', logger=stdoutlog, fmt='%(message)s', level_styles=levelStyles, field_styles=fieldStyles)
+
 main_bucket_part = args.main_bucket_name_part
+
+# Setup the s3 client with no request signing (no credentials used)
+s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+pager = s3_client.get_paginator('list_objects_v2')
+
+
+def _output_message(message, level):
+    if write_to_file:
+        flog.log(level=level, msg=message, )
+
+    stdoutlog.log(level=level, msg=message)
 
 
 def _get_back_off_time():
@@ -37,7 +94,7 @@ def _get_back_off_time():
 
 def _do_sleep():
     sleeper = _get_back_off_time()
-    print('Sleeping for {} seconds'.format(sleeper))
+    _output_message('Sleeping for {} seconds'.format(sleeper), logging.DEBUG)
     sleep(sleeper)
 
 
@@ -48,67 +105,102 @@ def _check_file_exists(source_file: str):
         return os.path.isfile(os.path.join(os.getcwd(), source_file))
 
 
-if not _check_file_exists(args.source_fuzzer_file):
-    print('No fuzzer file found')
-    exit(-1)
-
-if not _check_file_exists(args.source_separators_file):
-    print('No separators file found')
-    exit(-1)
-
-with open(args.source_fuzzer_file, 'r') as f:
-    fuzzers = [line.strip() for line in f]
-    fuzzers_count = len(fuzzers)
-
-with open(args.source_separators_file, 'r') as s:
-    separators = [line.strip() for line in s]
-    separators_count = len(separators)
-
-print(
-    f'Starting checks on target \'{main_bucket_part}\', loaded {fuzzers_count} fuzzer words from from {args.source_fuzzer_file} and loaded {separators_count} separators from {args.source_separators_file}.')
+def _list_bucket_contents(bucket_name):
+    try:
+        for data in pager.paginate(
+                Bucket=bucket_name,
+                FetchOwner=True,
+        ):
+            for keys in data["Contents"]:
+                _output_message(f'\t\t{keys["Key"]}', logging.INFO)
+    except botocore.exceptions.ClientError as ex:
+        if ex.response["Error"]["Code"] == "AccessDenied":
+            _output_message('\t\tList access denied', logging.WARN)
 
 
-def _print_public_bucket_info(check_name):
-    print('\tPublic bucket, try listing its contents: aws s3 ls s3://{} --no-sign-request'.format(check_name))
+def _print_public_bucket_info(bucket_name):
+    _output_message('\tPublic bucket, try listing its contents: aws s3 ls s3://{} --no-sign-request'.format(bucket_name), logging.INFO)
+    _list_bucket_contents(bucket_name=bucket_name)
 
 
-for separator in separators:
-    for fuzz in fuzzers:
-        check_name = "{}{}{}".format(main_bucket_part, separator, fuzz)
-        if len(check_name) > 63:
-            print('Bucket name ({}) is to long {}'.format(check_name, len(check_name)))
-            continue
+if __name__ == '__main__':
+    try:
+        _output_message("Run started @ {}".format(str(datetime.strftime(datetime.now(), '%d-%m-%Y %H:%M:%S.%f'))), logging.INFO)
+        if not _check_file_exists(args.source_fuzzer_file):
+            _output_message('No fuzzer file found', logging.ERROR)
+            exit(-1)
 
-        r = requests.head("http://{}.s3.amazonaws.com".format(check_name))
+        if not _check_file_exists(args.source_separators_file):
+            _output_message('No separators file found', logging.ERROR)
+            exit(-1)
 
-        if r.status_code != 404:
-            print('Success: {} --> {}'.format(check_name, r.status_code))
+        with open(args.source_fuzzer_file, 'r') as f:
+            fuzzers = [line.strip() for line in f]
+            fuzzers_count = len(fuzzers)
 
-            if r.status_code == 200:
-                _print_public_bucket_info(check_name)
+        with open(args.source_separators_file, 'r') as s:
+            separators = [line.strip() for line in s]
+            separators_count = len(separators)
 
+        _output_message(f'Starting checks on target \'{main_bucket_part}\', '
+                        f'loaded {fuzzers_count} fuzzer words from from {args.source_fuzzer_file} '
+                        f'and loaded {separators_count} separators from {args.source_separators_file}.', logging.INFO)
+
+        # check the base bucket
+        if len(main_bucket_part) > 63:
+            _output_message('Bucket name ({}) is to long {}'.format(main_bucket_part, len(main_bucket_part)), logging.ERROR)
         else:
-            # not a viable candidate
-            pass
+            r = requests.head("http://{}.s3.amazonaws.com".format(main_bucket_part))
 
-        if ENABLE_SLEEP_TIMER:
-            _do_sleep()
+            if r.status_code != 404:
+                _output_message('Found: {} --> {}'.format(main_bucket_part, r.status_code), logging.INFO)
 
-        # swap bucket part and fuzz
-        reversed_check_name = '{}{}{}'.format(fuzz, separator, main_bucket_part)
+                if r.status_code == 200:
+                    _print_public_bucket_info(main_bucket_part)
 
-        r = requests.head('http://{}.s3.amazonaws.com'.format(reversed_check_name))
+            else:
+                # not a viable candidate
+                pass
 
-        if r.status_code != 404:
-            print('Success: {} --> {}'.format(reversed_check_name, r.status_code))
+        for separator in separators:
+            for fuzz in fuzzers:
+                check_name = "{}{}{}".format(main_bucket_part, separator, fuzz)
+                if len(check_name) > 63:
+                    _output_message('Bucket name ({}) is to long {}'.format(check_name, len(check_name)), logging.INFO)
+                    continue
 
-            if r.status_code == 200:
-                _print_public_bucket_info(check_name)
-        else:
-            # not a viable candidate
-            pass
+                r = requests.head("http://{}.s3.amazonaws.com".format(check_name))
 
-        if ENABLE_SLEEP_TIMER:
-            _do_sleep()
+                if r.status_code != 404:
+                    _output_message('Exists: {} --> {}'.format(check_name, r.status_code), logging.INFO)
 
-print(f'All checks from \'{args.source_fuzzer_file}\' have completed.')
+                    if r.status_code == 200:
+                        _print_public_bucket_info(check_name)
+
+                else:
+                    # not a viable candidate
+                    pass
+
+                if ENABLE_SLEEP_TIMER:
+                    _do_sleep()
+
+                # swap bucket part and fuzz
+                reversed_check_name = '{}{}{}'.format(fuzz, separator, main_bucket_part)
+
+                r = requests.head('http://{}.s3.amazonaws.com'.format(reversed_check_name))
+
+                if r.status_code != 404:
+                    _output_message('Exists: {} --> {}'.format(reversed_check_name, r.status_code), logging.INFO)
+
+                    if r.status_code == 200:
+                        _print_public_bucket_info(check_name)
+                else:
+                    # not a viable candidate
+                    pass
+
+                if ENABLE_SLEEP_TIMER:
+                    _do_sleep()
+
+        _output_message(f'All checks from \'{args.source_fuzzer_file}\' have completed.', logging.INFO)
+    finally:
+        _output_message("Run ended @ {}".format(str(datetime.strftime(datetime.now(), '%d-%m-%Y %H:%M:%S.%f'))), logging.INFO)
